eee0995d2eb29394d02b186ff47bd854f540f7ba
sumit8b <mailme.sumit04@gmail.com>
2018-09-07 15:57:40 -0500

    File modified

--- src/test/java/com/sumit/todo/SpringToDoAppApplicationTests.java
+++ src/test/java/com/sumit/todo/SpringToDoAppApplicationTests.java
@@ -1,5 +1,10 @@
 package com.sumit.todo;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringToDoAppApplicationTests {
