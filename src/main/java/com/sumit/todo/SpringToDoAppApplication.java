7f97f61255a66f1a190969145150c581f4c566e8
sumit8b <mailme.sumit04@gmail.com>
2018-11-18 20:18:26 -0600

    Author name added

--- src/main/java/com/sumit/todo/SpringToDoAppApplication.java
+++ src/main/java/com/sumit/todo/SpringToDoAppApplication.java
@@ -1,5 +1,7 @@
 package com.sumit.todo;
-
+/**
+@author: Sumit Bhattacharjee
+**/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
